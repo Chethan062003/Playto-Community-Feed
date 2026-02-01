ad2ab38d1aa68c7db3c2b680e7cfa61d8c8c90ef
Chethan062003 <chethanns98811@gmail.com>
2026-02-01 22:45:08 +0530

    Update models.py

--- backend/feed/models.py
+++ backend/feed/models.py
@@ -1,21 +1,23 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     parent = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     post = models.ForeignKey(Post, null=True, blank=True, related_name='likes', on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, null=True, blank=True, related_name='likes', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
